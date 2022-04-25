6df2a0a5d47b6f63d8c06d7f21f99d1651642636
fahadnadeemx <85766937+fahadnadeemx@users.noreply.github.com>
2022-04-26 00:09:38 +0200

    update pom

--- src/main/java/com/book/bookmanage/controller/BookController.java
+++ src/main/java/com/book/bookmanage/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.book.bookmanage.controller;
 
+import com.book.bookmanage.entity.Book;
+import com.book.bookmanage.service.BookService;
+import com.book.bookmanage.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import javax.inject.Inject;
 
 @RestController
 @RequestMapping("/books")
@@ -18,7 +22,26 @@ public class BookController {
     private static final String REDIRECT = "redirect:/books";
     /* Injecting services of books in the controller */
 
+    @Autowired
+    private BookService bookService;
 
+    @Autowired
+    private IBookService iBookService;
+
+    @Inject
+    public BookController(BookService bookService, IBookService iBookService) {
+        this.bookService = bookService;
+        this.iBookService = iBookService;
+    }
+
+    /* Injecting services of category in the controller */
+
+    @GetMapping
+    public String getAllBooks(Model model) {
+        List<Book> list = bookService.loadAllBooks();
+        model.addAttribute("allBooks", list);
+        return "books";
+    }
 
 
 }
